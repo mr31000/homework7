fc5a1f2cf5959e6ef50815c14b17294f3543b206
mr31000 <mr3100@bs.amalnet.k12.il>
2025-01-31 06:30:58 +0200

    תמונות

--- app/src/main/java/com/example/homework7/MainActivity.java
+++ app/src/main/java/com/example/homework7/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.homework7;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +12,29 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
 
+public class MainActivity extends AppCompatActivity {
+public void btnClick (View view)
+{
+    ImageView iv1 = (ImageView) findViewById(R.id.iv);
+    ImageButton btn1 = (ImageButton) findViewById(R.id.btn);
+    Random random = new Random();
+    int randomNumber = random.nextInt(3) + 1;
+
+    if (randomNumber == 1) {
+        btn1.setImageResource(R.drawable.num1);
+        iv1.setImageResource(R.drawable.image1);
+    } else if (randomNumber == 2) {
+        btn1.setImageResource(R.drawable.num2);
+        iv1.setImageResource(R.drawable.image2);
+    } else if (randomNumber == 3) {
+        btn1.setImageResource(R.drawable.num3);
+        iv1.setImageResource(R.drawable.image3);
+    }
+
+    iv1.setVisibility(View.VISIBLE);
+}
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
